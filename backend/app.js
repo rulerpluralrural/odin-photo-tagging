95c2543d87b2cb4abebeb962d491c1161b018972
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2024-02-02 22:07:42 +0800

    Added render URL on .env

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
 	cors({
-		origin: ["http://localhost:5173"],
+		origin: ["http://localhost:5173", process.env.FRONTEND_URL],
 		methods: ["GET", "POST", "PUT", "DELETE"],
 	})
 );
